8fe47386aea02467b8c58a450bc51378230ca936
Fa-Di <50700963+Fa-Di@users.noreply.github.com>
2024-05-26 17:06:00 +0100

    Change main.py

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ W2G_API_KEY = os.getenv('W2G_API_KEY')
 # Initialize the database connection
 conn = sqlite3.connect('movies.db')
 c = conn.cursor()
+c.execute('''DROP TABLE IF EXISTS movies''')
 
 # Create a table if it doesn't exist
 c.execute('''CREATE TABLE IF NOT EXISTS movies (
+                guild_id TEXT NOT NULL,
                 user_id TEXT,
                 movie_name TEXT,
                 date_added TEXT
@@ -36,7 +38,7 @@ class MyBot(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.default())
         self.tree = app_commands.CommandTree(self)
-
+        
     async def on_ready(self):
         await self.tree.sync()
         print(f'{self.user} has connected to Discord!')
@@ -56,7 +58,9 @@ bot = MyBot()
 async def add_movie(interaction: discord.Interaction, movie_name: str):
     user_id = str(interaction.user.id)
     date_added = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    c.execute("INSERT INTO movies (user_id, movie_name, date_added) VALUES (?, ?, ?)", (user_id, movie_name, date_added))
+
+    guild_id = str(interaction.guild.id)  # Add interaction before guild
+    c.execute("INSERT INTO movies (guild_id, user_id, movie_name, date_added) VALUES (?, ?, ?, ?)", (guild_id, user_id, movie_name, date_added))  # Add guild_id parameter
     conn.commit()
     embed = discord.Embed(title="Movie Added", description=f'"{movie_name}" has been added to your plan to watch list!', color=discord.Color.blue())
     await interaction.response.send_message(embed=embed)
@@ -65,7 +69,8 @@ async def add_movie(interaction: discord.Interaction, movie_name: str):
 @bot.tree.command(name='deletemovie', description='Delete a movie from your plan to watch list')
 async def delete_movie(interaction: discord.Interaction, movie_name: str):
     user_id = str(interaction.user.id)
-    c.execute("DELETE FROM movies WHERE user_id = ? AND movie_name = ?", (user_id, movie_name))
+    guild_id = str(interaction.guild.id)
+    c.execute("DELETE FROM movies WHERE user_id = ? AND movie_name = ? AND guild_id = ?", (user_id, movie_name,guild_id))
     conn.commit()
     if c.rowcount > 0:
         embed = discord.Embed(title="Movie Removed", description=f'"{movie_name}" has been removed from your plan to watch list!', color=discord.Color.orange())
@@ -78,7 +83,8 @@ async def delete_movie(interaction: discord.Interaction, movie_name: str):
 @bot.tree.command(name='clearmovies', description='Clear your plan to watch list')
 async def clear_movies(interaction: discord.Interaction):
     user_id = str(interaction.user.id)
-    c.execute("DELETE FROM movies WHERE user_id = ?", (user_id,))
+    guild_id = str(interaction.guild.id)
+    c.execute("DELETE FROM movies WHERE user_id = ? AND guild_id = ?", (user_id,guild_id))
     conn.commit()
     embed = discord.Embed(title="List Cleared", description='Your plan to watch list has been cleared!', color=discord.Color.purple())
     await interaction.response.send_message(embed=embed)
@@ -87,7 +93,8 @@ async def clear_movies(interaction: discord.Interaction):
 @bot.tree.command(name='listmovies', description='List all movies in your plan to watch list')
 async def list_movies(interaction: discord.Interaction):
     user_id = str(interaction.user.id)
-    c.execute("SELECT movie_name, date_added FROM movies WHERE user_id = ?", (user_id,))
+    guild_id = str(interaction.guild.id)
+    c.execute("SELECT movie_name, date_added FROM movies WHERE user_id = ? AND guild_id = ?", (user_id,guild_id))
     movies = c.fetchall()
     if not movies:
         embed = discord.Embed(title="No Movies", description='Your plan to watch list is empty!', color=discord.Color.red())
@@ -132,22 +139,22 @@ async def search(interaction: discord.Interaction, query:str):
         results = getMovies(query)
         await interaction.response.send_message("Searching movies...", ephemeral=True)
         
-        # Assembler les images
+        # Assemble the images
         images = [Image.open(BytesIO(requests.get(movie['cover']).content)) for movie in results]
         combined_image = MovieView.combine_images(images)
         with BytesIO() as image_binary:
             combined_image.save(image_binary, "JPEG")
             image_binary.seek(0)
-            await interaction.followup.send(file=discord.File(fp=image_binary, filename='combined_image.jpg'))
-        # Créer l'embed avec les résultats de recherche
-        embed = discord.Embed(title="Movie Results")
+            file = discord.File(fp=image_binary, filename='combined_image.jpg')
+        
+        # Create the embed with the search results
+        
         view = discord.ui.View()
         for result in results:
-            
             view.add_item(discord.ui.Button(label='Go to ' + result['title'], url=make_room(result['link'])))
-            
-        # Envoyer l'embed avec l'image assemblée
-        await interaction.followup.send(embed=embed, view=view)
+        
+        # Send the embed with the assembled image and the search results
+        await interaction.followup.send(file=file, view=view)
         
 
         print(f'search from {user_id}')

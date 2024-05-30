2d6e93243ab1514b96246f4b2442bd601fcaf1d6
Fa-Di <50700963+Fa-Di@users.noreply.github.com>
2024-05-30 18:57:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sqlite3
 from PIL import Image
 from io import BytesIO
 from datetime import datetime
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 import discord
 from discord.ext import commands
@@ -20,7 +20,7 @@ from keep_it_alive import keep_alive
 
 
 
-load_dotenv('envBot.env')
+#load_dotenv('envBot.env')
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 W2G_API_KEY = os.getenv('W2G_API_KEY')
 
